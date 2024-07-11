eff31de7b9046143ce7350ef18cc48bcfb103686
Haseeb Khatri <153817119+haseebkhatri009@users.noreply.github.com>
2024-07-11 21:51:21 +0500

    Update app.js

--- app.js
+++ app.js
@@ -1,41 +1,121 @@
-let cardId = 0;
+const main = document.querySelector("#main-div");
+const addCardBtn = document.querySelector("#addCard");
 
-function allowDrop(ev) {
-ev.preventDefault();
+let element = null;
+
+const addTask = (event) => {
+event.preventDefault();
+
+const currentForm = event.target;
+const value = currentForm.elements[0].value;
+const parent = currentForm.parentElement;
+const ticket = createTicket(value);
+
+if (!value) return;
+
+parent.insertBefore(ticket, currentForm);
+
+const h3Value = parent.children[0].innerText;
+
+if (!Array.isArray(savedTasks[h3Value])) {
+    savedTasks[h3Value] = [];
 }
 
-function drag(ev) {
-ev.dataTransfer.setData("text", ev.target.id);
+savedTasks[h3Value].push(value);
+
+currentForm.reset();
+};
+
+const myCreateCard = (cardTitle) => {
+const myDiv = document.createElement("div");
+const myH3 = document.createElement("h3");
+const myForm = document.createElement("form");
+const myInput = document.createElement("input");
+const nBtn = document.createElement("button");
+
+nBtn.textContent = "Enter";
+nBtn.setAttribute("class", "nBtn");
+
+const h3Text = document.createTextNode(cardTitle);
+
+myDiv.setAttribute("class", "column");
+myInput.setAttribute("type", "text");
+myInput.setAttribute("placeholder", "add task");
+
+myH3.appendChild(h3Text);
+myForm.appendChild(myInput);
+myDiv.appendChild(myH3);
+myDiv.appendChild(myForm);
+myForm.appendChild(nBtn); 
+
+myForm.addEventListener("submit", addTask);
+
+myDiv.addEventListener("dragleave", (event) => event.preventDefault());
+myDiv.addEventListener("dragover", (event) => event.preventDefault());
+
+myDiv.addEventListener("drop", (event) => {
+const dropper = event.target;
+
+    if (dropper.className.includes("column")) {
+        dropper.appendChild(element);
+    }
+
+    if (dropper.className.includes("ticket")) {
+        dropper.parentElement.appendChild(element);
+    }
+});
+
+return myDiv;
+};
+
+const createTicket = (value) => {
+const ticket = document.createElement("div");
+ticket.setAttribute("class", "ticket");
+
+const todoText = document.createElement("span");
+todoText.textContent = value;
+
+const deleteBtn = document.createElement("button");
+deleteBtn.textContent = "delete";
+deleteBtn.setAttribute("class", "delete-todo-btn");
+
+deleteBtn.addEventListener("click", () => {
+    const todoToDelete = deleteBtn.parentNode;
+    todoToDelete.remove();
+
+});
+
+ticket.appendChild(todoText);
+ticket.appendChild(deleteBtn);
+
+ticket.setAttribute("draggable", "true");
+
+ticket.addEventListener("mousedown", (event) => {
+    element = event.target;
+});
+
+return ticket;
+};
+
+let savedTasks = JSON.parse(localStorage.getItem("saveTasks")) || {};
+
+for (const title in savedTasks) {
+    const card = myCreateCard(title);
+
+    savedTasks[title].forEach((taskValue) => {
+    const p = createTicket(taskValue);
+    card.insertBefore(p, card.lastElementChild.previousSibling); // Insert before the form in card
+});
+
+    main.insertBefore(card, addCardBtn);
 }
 
-function drop(ev) {
-ev.preventDefault();
-const data = ev.dataTransfer.getData("text");
-const card = document.getElementById(data);
-const newList = ev.target.closest('.list-cards');
-newList.appendChild(card);
-}
+addCardBtn.addEventListener("click", () => {
+    const cardTitle = prompt("Please Enter Card Name?");
 
-function addCard(listId, inputId) {
-const inputText = document.getElementById(inputId).value.trim();
-if (inputText !== '') {
-    const card = createCardElement(inputText);
-    document.getElementById(listId).appendChild(card);
-    document.getElementById(inputId).value = '';
-}
-}
+    if (!cardTitle) return;   
 
-function createCardElement(text) {
-const card = document.createElement('div');
-const cardIdAttr = `card-${cardId++}`;
-card.id = cardIdAttr;
-card.className = 'card';
-card.draggable = true;
-card.addEventListener('dragstart', drag);
+    const yourDiv = myCreateCard(cardTitle);
 
-card.innerHTML = `
-    <div class="card-content">${text}</div>
-;
-
-return card;
-}
+main.insertBefore(yourDiv, addCardBtn);
+});
